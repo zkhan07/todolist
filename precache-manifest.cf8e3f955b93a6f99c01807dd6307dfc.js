5fb1aba8c9929270816a98fa839023a0ca06aa2f
zaid <khanzaid786786.kz@gmail.com>
2019-10-04 16:08:06 +0530

    Updates

---
+++ precache-manifest.cf8e3f955b93a6f99c01807dd6307dfc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d105fd86ddcde0e39711352b2d40bb4",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "1db9d5f15fb6ac74e890",
+    "url": "/todolist/static/js/2.49cab131.chunk.js"
+  },
+  {
+    "revision": "90e168f039e448fa1f96",
+    "url": "/todolist/static/js/main.218e7458.chunk.js"
+  },
+  {
+    "revision": "15eb0bb8eddbf4004f28",
+    "url": "/todolist/static/js/runtime-main.63612fe0.js"
+  }
+]);
